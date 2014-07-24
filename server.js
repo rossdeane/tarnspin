c1b572b4f118d6e3ca39362a9f08c35947da3a36
Ross Deane <ross.deane@gmail.com>
2014-07-24 16:51:38 +0100

    tarn

--- server.js
+++ server.js
@@ -3,14 +3,9 @@ var app       = express();
 var fs        = require("fs");
 var swig      = require('swig');
 var bodyParser   = require('body-parser');
-var basicAuth    = require('basic-auth-connect');
 
 app.use(express.static('public'));
 app.use(bodyParser());
-
-var auth_user = process.env.AUTH_USER || 'duedil';
-var auth_pass = process.env.AUTH_PASS || 'duedil!!9001';
-app.use(basicAuth(auth_user, auth_pass));
 
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
